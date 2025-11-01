5e665dc5e03ec65cde9f098301f1bdb918a22e5f
Sahilshinde123 <sahilshinde3377@gmail.com>
2025-11-01 11:23:27 +0530

    Fix deployment error by updating vite config

--- MERN-Fullstack-Expense-Tracker-main/FrontEnd/vite.config.js
+++ MERN-Fullstack-Expense-Tracker-main/FrontEnd/vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          ui: ['react-icons', 'emoji-picker-react'],
+          charts: ['recharts'],
+          utils: ['axios', 'moment'],
+        },
+      },
+    },
+  },
 });
